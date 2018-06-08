0dcb411625a1632f745f768c402066f297c35f7b
kotaro.kawashiki <kotaro.kawashiki@rakuten.com>
2018-06-08 02:41:24 +0000

    follow, unfollow

--- app/User.php
+++ app/User.php
@@ -7,28 +7,100 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+        use Notifiable;
     
-    public function microposts()
-    {
-        return $this->hasMany(Micropost::class);
+        /**
+         * The attributes that are mass assignable.
+         *
+         * @var array
+         */
+        protected $fillable = [
+            'name', 'email', 'password',
+        ];
+    
+        /**
+         * The attributes that should be hidden for arrays.
+         *
+         * @var array
+         */
+        protected $hidden = [
+            'password', 'remember_token',
+        ];
+        
+        public function microposts()
+        {
+            return $this->hasMany(Micropost::class);
+        }
+        
+        public function followings($id)
+        {
+            $user = User::find($id);
+        $followings = $user->followings()->paginate(10);
+
+        $data = [
+            'user' => $user,
+            'users' => $followings,
+        ];
+
+        $data += $this->counts($user);
+
+        return view('users.followings', $data);
+        }
+    
+        public function followers($id)
+        {
+             $user = User::find($id);
+        $followers = $user->followers()->paginate(10);
+
+        $data = [
+            'user' => $user,
+            'users' => $followers,
+        ];
+
+        $data += $this->counts($user);
+
+        return view('users.followers', $data);
+        }
+        
+        public function follow($userId)
+        {
+        // confirm if already following
+        $exist = $this->is_following($userId);
+        // confirming that it is not you
+        $its_me = $this->id == $userId;
+    
+        if ($exist || $its_me) {
+            // do nothing if already following
+            return false;
+            } 
+        else {
+            // follow if not following
+            $this->followings()->attach($userId);
+            return true;
+              }
+        }
+    
+        public function unfollow($userId)
+        {
+        // confirming if already following
+        $exist = $this->is_following($userId);
+        // confirming that it is not you
+        $its_me = $this->id == $userId;
+    
+    
+        if ($exist && !$its_me) {
+            // stop following if following
+            $this->followings()->detach($userId);
+            return true;
+            } 
+        else {
+            // do nothing if not following
+            return false;
+            }
+        }
+    
+    
+    public function is_following($userId) {
+        return $this->followings()->where('follow_id', $userId)->exists();
     }
 }
