09aa92ff253e61a6ae6530e9bb7e98023ba42337
kotaro.kawashiki <kotaro.kawashiki@rakuten.com>
2018-06-12 04:05:49 +0000

    change favorite button

--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -12,21 +12,7 @@
             <div>
                 <p>{!! nl2br(e($micropost->content)) !!}</p>
             </div>
-            <div class="btn-toolbar">
-                 @if (Auth::user()->is_favoriting($micropost->id))
-                 <div class="btn-group">
-                     {!! Form::open(['route' => ['micropost.unfavorite', $micropost->id], 'method' => 'delete']) !!}
-                        {!! Form::submit('Unfavorite', ['class' => "btn btn-danger btn-xs"]) !!}
-                     {!! Form::close() !!}
-                 </div>
-                 @else
-                 <div class="btn-group">
-                     {!! Form::open(['route' => ['micropost.favorite', $micropost->id]]) !!}
-                        {!! Form::submit('Favorite', ['class' => "btn btn-primary btn-xs"]) !!}
-                     {!! Form::close() !!}
-                 </div>
-                 @endif
-            </div>
+            @include('user_favorite.favorite_button', ['user' => $user])
             <div>
                 @if (Auth::user()->id == $micropost->user_id)
                     {!! Form::open(['route' => ['microposts.destroy', $micropost->id], 'method' => 'delete']) !!}
